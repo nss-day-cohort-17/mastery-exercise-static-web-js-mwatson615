937dbef42dafb276ef4cfbc050120b477eb743ed
Michael Watson <m.watson.tn@gmail.com>
2016-12-02 15:58:54 -0600

    commented out events, mods to function

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 console.log("HI!")
 
-document.getElementById("treeButton").addEventListener("click", function makeTree(foliage, height) {
-	if (foliage === "" || height === "") {
+// var foliage = document.getElementById("letter")
+// var height = document.getElementById("number")
+
+// document.getElementById("treeButton").addEventListener("click", makeTree() {
+// })
+
+// foliage.addEventListener("keyup", makeTree() {
+// })
+
+// height.addEventListener("keyup", makeTree() {
+
+// })
+
+/*
+var foliage = document.getElementById("letter")
+var height = document.getElementById("number")
+*/
+
+var treeChars = {"foliage" : "*", "height" : 5}
+
+function makeTree() {
+	if (treeChars.foliage === null || treeChars.height === null) {
 		alert("Both fields must have a value")
 	}
-})
-
-
-
-var foliage = document.getElementById("letter").value;
-var height = document.getElementById("number").value;
-
-var treeChars = {"foliage" : foliage, "height" : height}
-
-function makeTree(treeChars) {
-	for (i = 1; i < height; i++) {
-		height + 2;
-		foliage * height;
+	var treeLetter = treeChars.foliage;
+	var treeNum = treeChars.height;
+	var treeSpaces = " "
+	for (i = 1; i <= treeNum; i++) {
+		treeLetter = i + 2;
+		treeSpaces = treeNum - 1;
+		treeNum.repeat(i + 2);
+		console.log(treeSpaces + treeLetter.repeat(i + 2));
 	}
 }
-	console.log(treeChars)
+
+makeTree()
