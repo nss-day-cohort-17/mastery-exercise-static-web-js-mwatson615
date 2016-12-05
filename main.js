0b7fe8c619931f4f0b20c40efe3b9c78794b765d
Michael Watson <m.watson.tn@gmail.com>
2016-12-04 22:24:57 -0600

    doesn't work, don't know why

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
 console.log("HI!")
 
-// var foliage = document.getElementById("letter")
-// var height = document.getElementById("number")
 
-// document.getElementById("treeButton").addEventListener("click", makeTree() {
-// })
 
-// foliage.addEventListener("keyup", makeTree() {
-// })
+var foliage = document.getElementById("letter").value;
+var height = document.getElementById("number").value;
+var addButton = document.getElementById("treeButton");
 
-// height.addEventListener("keyup", makeTree() {
+addButton.addEventListener("click", makeTree());
+foliage.addEventListener("keyup", makeTree());
+height.addEventListener("keyup", makeTree());
 
-// })
 
-/*
-var foliage = document.getElementById("letter")
-var height = document.getElementById("number")
-*/
 
-var treeChars = {"foliage" : "*", "height" : 5}
+var treeChars = {"foliage" : foliage , "height" : height}
 
-function makeTree() {
-	/*
-	if (treeChars.foliage === null || treeChars.height === null) {
-		alert("Both fields must have a value")
+var treeLetter = treeChars.foliage;
+var treeNum = treeChars.height;
+var treeSpaces = " "
+
+function makeTree(treeChars) {
+	if (KeyboardEvent.key === 13 && foliage != null && height != null) {
+		for (i = 1; i <= treeNum+i; i++) {
+			if (treeNum < treeNum+i) {
+			--treeNum;
+			console.log(treeSpaces.repeat(treeNum) + treeLetter.repeat(i++));
+	} else {
+		alert("Both fields must have a value");
 	}
-	*/
-	var treeLetter = treeChars.foliage;
-	var treeNum = treeChars.height;
-	var treeSpaces = " "
-	for (i = 1; i <= treeNum+i; i++) {
-		console.log(treeSpaces.repeat(--treeNum) + treeLetter.repeat(i++));
-	}
-
 }
-
+}
+}
 makeTree();
