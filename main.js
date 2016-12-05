8ff7027e499c221f9b99763169086d38d6a2be27
Michael Watson <m.watson.tn@gmail.com>
2016-12-05 11:00:30 -0600

    still not working

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
+
 var foliage = document.getElementById("letter").value;
 var height = document.getElementById("number").value;
 var addButton = document.getElementById("treeButton");
 
-addButton.addEventListener("click", makeTree());
-foliage.addEventListener("keyup", makeTree());
-height.addEventListener("keyup", makeTree());
+addButton.addEventListener("click", makeTree);
+foliage.addEventListener("keyup", makeTree);
+height.addEventListener("keyup", makeTree);
 
 
 
-var treeChars = {"foliage" : "*" , "height" : 5}
+var treeChars = {"foliage" : foliage , "height" : height}
 
 var treeLetter = treeChars.foliage;
 var treeNum = treeChars.height;
 var treeSpaces = " "
 var treeWidth = 0;
 
-function makeTree(treeChars) {
-	if (KeyboardEvent.key !== 13 || (foliage == null && height == null)) {
-		alert("Both fields must have a value");
-	} else {
-		for (i = 1; i <= treeNum; i+1) {
+function makeTree() {
+	for (i = 1; i <= treeNum; i+1) {
 			--treeNum;
 			treeWidth++;
 			console.log(treeSpaces.repeat(treeNum) + treeLetter.repeat(treeWidth++));
 	}
+	if (foliage == null || height == null) {
+		alert("Both fields must have a value");
 	}
 }
+//KeyboardEvent.key !== 13
 
 makeTree();
