c605c03ed623c8c93a0118ff1e66f87c678c7251
Michael Watson <m.watson.tn@gmail.com>
2016-12-07 22:06:11 -0600

    moved fixed listeners for ent and fine tuned maketree loop, it works git status cd .. ls \d " !

--- main.js
+++ main.js
@@ -5,35 +5,38 @@ var heightInput = document.getElementById("number");
 var addButton = document.getElementById("treeButton");
 
 addButton.addEventListener("click", other);
-// foliage.addEventListener("keyup", makeTree);
-// height.addEventListener("keyup", makeTree);
+foliageInput.addEventListener("keypress", entKey);
+heightInput.addEventListener("keypress", entKey);
 
 
 
-var treeChars = {"foliage" : "" , "height" : ""}
 
-var treeSpaces = " "
-var treeWidth = 0;
+
+function entKey(e) {
+	if (e.code === "Enter") {
+		other();
+	}
+}
 
 
 function other() {
+	var treeChars = {"foliage" : "" , "height" : ""}
 	treeChars.foliage = foliageInput.value;
 	treeChars.height = heightInput.value;
-	
+
 	if (treeChars.foliage === "" || treeChars.height === "") {
 		alert("Both fields must have a value");
-	}
+	} else {
 	makeTree(treeChars);
+}
 }
 
 function makeTree(a) {
-
+	var treeSpaces = " "
+	var treeWidth = 0;
 	for (i = 1; i <= a.height; i+1) {
 			--a.height;
 			treeWidth++;
 			console.log(treeSpaces.repeat(a.height) + a.foliage.repeat(treeWidth++));
 		}
 }
-//KeyboardEvent.key !== 13
-
-makeTree(treeChars);
